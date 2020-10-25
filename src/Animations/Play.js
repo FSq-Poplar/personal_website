644f8b6a33128f6c6b861039ac6d504981ad515a
Poplar Wang <wang.poplar@gmail.com>
2020-10-25 01:57:28 -0400

    Code styling

--- src/Animations/Play.js
+++ src/Animations/Play.js
@@ -1,23 +1,20 @@
-import { TimelineMax as Timeline, Power1 } from 'gsap';
+import { TimelineMax as Timeline, Power1 } from 'gsap'
 import { getProjectTimeline, getHomeTimeline } from './Animations'
 
 export const play = (pathname, node, appears) => {
-  const delay = appears ? 0 : 0.5;
+  const delay = appears ? 0 : 0.5
   let timeline
 
-  if (pathname === '/')
-    timeline = getHomeTimeline(node, delay);
-  else
-    timeline = getProjectTimeline(node, delay);
+  if (pathname === '/') { timeline = getHomeTimeline(node, delay) } else { timeline = getProjectTimeline(node, delay) }
 
   window
     .loadPromise
-    .then(() => requestAnimationFrame(() => timeline.play()))
+    .then(() => window.requestAnimationFrame(() => timeline.play()))
 }
 
 export const exit = (node) => {
-  const timeline = new Timeline({ paused: true });
+  const timeline = new Timeline({ paused: true })
 
-  timeline.to(node, 0, { autoAlpha: 0, ease: Power1.easeOut });
-  timeline.play();
+  timeline.to(node, 0, { autoAlpha: 0, ease: Power1.easeOut })
+  timeline.play()
 }
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Transition, TransitionGroup } from 'react-transition-group';
+import { Transition, TransitionGroup } from 'react-transition-group'
 import './index.css'
 import './App.css'
 
@@ -15,30 +15,31 @@ export default class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
-      <div className='App'>
-        <Route render={({location}) => {
-          const { pathname, key } = location
-          return (
-            <TransitionGroup component={null}>
-              <Transition
-                key={key}
-                appear={true}
-                onEnter={(node, appears) => play(pathname, node, appears)}
-                onExit={(node, appears) => exit(node, appears)}
-                timeout={{enter: 750, exit: 150}}
-              >
-                <Switch location={location}>
-                  <Route exact path='/' component={Home} />
-                  <Route path='/ppobo' component={PpoBO} />
-                  <Route path='/fpgann' component={FPGANN} />
-                  <Route path='/builds' component={Builds} />
-                  <Route path='/miniprojects' component={MiniProjects} />
-                </Switch>
-              </Transition>
-            </TransitionGroup>
-          )
-        }}/>
-      </div>
+        <div className='App'>
+          <Route render={({ location }) => {
+            const { pathname, key } = location
+            return (
+              <TransitionGroup component={null}>
+                <Transition
+                  key={key}
+                  appear
+                  onEnter={(node, appears) => play(pathname, node, appears)}
+                  onExit={(node, appears) => exit(node, appears)}
+                  timeout={{ enter: 750, exit: 150 }}
+                >
+                  <Switch location={location}>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/ppobo' component={PpoBO} />
+                    <Route path='/fpgann' component={FPGANN} />
+                    <Route path='/builds' component={Builds} />
+                    <Route path='/miniprojects' component={MiniProjects} />
+                  </Switch>
+                </Transition>
+              </TransitionGroup>
+            )
+          }}
+          />
+        </div>
       </BrowserRouter>
     )
   }
